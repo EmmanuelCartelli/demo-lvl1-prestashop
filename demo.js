4b239e97c734fc7364209e237f159bccba591f33
Emmanuel <emmanuel@lvl1.fr>
2021-10-14 16:10:16 +0200

    v1.0

---
+++ demo.js
@@ -0,0 +1,65 @@
+let buttonSwitch = document.querySelector(".switch");
+let logoWP = ""
+document.querySelector("img").setAttribute("src", "https://www.lvl1.fr/wp-content/uploads/2021/03/site-65.png");
+document.querySelector(".loader img").setAttribute("src", "https://www.lvl1.fr/wp-content/uploads/2021/03/Logo-V3-noir.png");
+
+function loader(){
+    setTimeout(function(){
+        document.querySelector(".loader").setAttribute("style", "display:none;")
+    }, 3000)
+    document.querySelector(".loader").setAttribute("style", "display:flex;")
+}
+
+
+
+function urlID(){
+    return new URL(location.href).searchParams.get("name")
+}
+
+function getName(parametres){
+    let nameWP = "wordpress";
+    let namePS = "prestashop";
+        if(parametres == nameWP){
+            buttonSwitch.setAttribute("style", "background:#ff0075;")    
+            document.querySelector("iframe").setAttribute("src", "https://webdemo.lvl1.fr/");
+            buttonSwitch.textContent = "Demo Prestashop";    
+        }
+        else if(parametres == namePS){
+            buttonSwitch.setAttribute("style", "background: linear-gradient(45deg, #0073aa, #0085ba);") 
+            document.querySelector("iframe").setAttribute("src", "https://prestademo.lvl1.fr/");
+            buttonSwitch.textContent = "Demo Wordpress";
+        }
+        else{
+            document.querySelector("body").style = "display: none;"
+        }
+}
+
+function main(){
+    let nameCMS = urlID();
+    getName(nameCMS);
+    loader();
+    buttonSwitch.addEventListener("click", function(){
+        loader();
+        if(nameCMS == "wordpress"){
+            document.querySelector("iframe").setAttribute("src", "https://prestademo.lvl1.fr/");
+            setTimeout(function(){
+                buttonSwitch.textContent = "Demo Wordpress";  
+                buttonSwitch.setAttribute("style", "background: linear-gradient(45deg, #0073aa, #0085ba);")
+            },1500)
+            nameCMS = "prestashop";
+        }
+        else{
+            document.querySelector("iframe").setAttribute("src", "https://webdemo.lvl1.fr/");
+            setTimeout(function(){
+                buttonSwitch.textContent = "Demo Prestashop";
+                buttonSwitch.setAttribute("style", "background:#ff0075;")    
+            },1500)
+            nameCMS = "wordpress";
+        }
+    })
+}
+
+main();
+
+
+
